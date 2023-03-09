d6969796d9e2c78bf56d9ec95e9048d6edf73b10
Denise Schirmer <denise_liima@hotmail.com>
2023-03-09 08:39:39 -0300

    added functionalities

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  url: "https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions",
+  headers: {
+    "X-RapidAPI-Key": "606f0af189msh9d89efe3597153fp1cbbcejsn5405672abf01",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "";
